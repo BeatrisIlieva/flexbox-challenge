bf2ec538a818060fa9f641d41c3b26147f2d29cb
Beatris Ilieve <beatrisilieve@icloud.com>
2025-01-23 22:01:12 +0200

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -51,3 +51,62 @@ propertyContainerElements.forEach(propertyContainerElement => {
         }
     });
 });
+
+
+
+
+
+
+const boxesContainerElements = document.querySelectorAll('.boxes-container');
+
+boxesContainerElements.forEach(boxesContainerElement => {
+    boxesContainerElement.addEventListener('click', e => {
+        const carouselElement = e.currentTarget;
+        const buttonElement = e.target;
+
+        // const ulElement = carouselElement.querySelector('ul');
+        // const liElements = Array.from(ulElement.querySelectorAll('li'));
+
+        if (buttonElement.tagName === 'BUTTON') {
+            const buttonId = buttonElement.id;
+
+            return functionMapper[buttonId](boxesContainerElements);
+        }
+    });
+
+    const functionMapper = {
+        next: boxesContainerElements => displayNextElement(boxesContainerElements),
+        previous: boxesContainerElements => displayPreviousElement(boxesContainerElements),
+    };
+});
+
+function displayNextElement(elements) {
+    for (let i = 0; i < elements.length; i++) {
+        let currentElement = elements[i];
+
+        if (window.getComputedStyle(currentElement).display === 'flex') {
+            currentElement.style.display = 'none';
+
+            const nextIndex = (i + 1) % elements.length;
+            elements[nextIndex].style.display = 'flex';
+
+            break;
+        }
+    }
+}
+
+function displayPreviousElement(elements) {
+    for (let i = elements.length - 1; i >= 0; i--) {
+        let currentElement = elements[i];
+
+        if (window.getComputedStyle(currentElement).display === 'flex') {
+            currentElement.style.display = 'none';
+
+            const previousIndex = (i - 1 + elements.length) % elements.length;
+            elements[previousIndex].style.display = 'flex';
+
+            break;
+        }
+    }
+}
+
